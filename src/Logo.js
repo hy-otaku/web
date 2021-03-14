849c1c8c761464aa92a86e7a2b1367835ba21c14
tado-mi <tadouchiha@gmail.com>
2021-03-13 23:05:08 -0500

    feature(logo): created the component (#2)

---
+++ src/Logo.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Logo extends Component {
+  render () {
+    return (
+      <>
+        հայերեն անիմե ու մանգա
+      </>
+    )
+  }
+}
+
+export default Logo
