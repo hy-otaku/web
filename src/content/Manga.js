4942282fb6e4a04734ceeec635b8efd24f83bf35
tado-mi <tadouchiha@gmail.com>
2021-03-13 20:10:55 -0500

    fix(headers): removed the search bar and the heading from `<Anime />` and `<Manga />` components

--- src/content/Manga.js
+++ src/content/Manga.js
@@ -1,10 +1,6 @@
 import React, { Component } from 'react'
 
-import { mangaJson } from '../constants.js'
-
 import './sass/style.scss'
-
-import { Input } from 'antd'
 
 import Content, { getRoutes as contentRoutes } from './manga/Content.js'
 
@@ -13,41 +9,14 @@ export const getRoutes = () => {
 }
 
 class Manga extends Component {
-  constructor (props) {
-    super(props)
-    this.data = mangaJson
-    this.state = {
-      manga: ''
-    }
-    this.searchPlaceholder = 'ո՞ր մանգան ես փնտրում․․․'
-  }
-
-  onSearch (value) {
-    this.setState({ manga: value })
-  }
-
-  onChange ({ target: { value } }) {
-    this.setState({ manga: value })
-  }
-
   render () {
-    const { Search } = Input
-    const { manga } = this.state
+    const { query } = this.props
 
     return (
       <div className='main-content'>
 
-        <h1> մանգա
-          <Search
-            enterButton allowClear
-            placeholder={this.searchPlaceholder}
-            onSearch={this.onSearch.bind(this)}
-            onChange={this.onChange.bind(this)}
-          />
-        </h1>
-
         <Content
-          searchValue={manga}
+          searchValue={query}
         />
 
       </div>
