82e092d121af36fadd73a143a226e10df3b478e5
tado-mi <tadouchiha@gmail.com>
2021-03-14 12:26:22 -0400

    feature(collapse-piccha): piccha self-collapses on `/anime` and `/manga` urls (#7)

--- src/Piccha.js
+++ src/Piccha.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { withRouter } from 'react-router-dom'
 
 import './sass/Piccha.scss'
 
@@ -6,22 +7,27 @@ import Navigator from './Navigator.js'
 
 class Piccha extends Component {
   render () {
-    const { sections, searchFunction } = this.props
+    const { sections, searchFunction, location: { pathname } } = this.props
+    const collapsed = pathname.startsWith('/anime') || pathname.startsWith('/manga')
     return (
-      <div className='piccha'>
+      <div className={`piccha${collapsed? ' collapsed': ''}`}>
 
         <Navigator sections={sections} searchFunction={searchFunction} />
 
-        <div className='info'>
-          <h1> Lorem ipsum </h1>
-          <span>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-          </span>
-        </div>
+        {
+          !collapsed && (
+            <div className='info'>
+              <h1> Lorem ipsum </h1>
+              <span>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </span>
+            </div>
+          )
+        }
 
       </div>
     )
   }
 }
 
-export default Piccha
+export default withRouter(Piccha)
