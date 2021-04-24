6a783d8bfb6903ac1a1f9a37cbe9b1fe7b4fa9be
tado-mi <tadouchiha@gmail.com>
2021-04-24 16:56:26 -0400

    feature(archive): did the set up

---
+++ src/content/Archive.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+import './sass/style.scss'
+
+class Archive extends Component {
+  render () {
+    return (
+      <div className='main-content'>
+        արխիվ
+      </div>
+
+    )
+  }
+}
+
+export default Archive
