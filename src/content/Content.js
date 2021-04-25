7284d2b090e2e767c2c5e3fa82adfbd909320efd
tado-mi <tadouchiha@gmail.com>
2021-04-24 22:57:04 -0400

    feature(archive): some refactoring and generalization

--- src/content/Content.js
+++ src/content/Content.js
@@ -3,13 +3,21 @@ import { withRouter } from 'react-router-dom'
 
 import { sortBy } from 'lodash'
 
-import { animeJson, mangaJson } from '../constants.js'
+import { animeJson, mangaJson, archiveJson } from '../constants.js'
 
 import './sass/style.scss'
 
 import Card from './util/Card.js'
+import Custombox from './util/Custombox.js'
 
 class Content extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      open: false
+    }
+  }
+
   byGenre (item) {
     const { genre } = this.props
     if (!genre) { // ignore genre when none is selected
@@ -66,10 +74,10 @@ class Content extends Component {
   render () {
     const { path: self } = this.props.match
 
-    const { query, anime, manga, genreFunc } = this.props
-    const json = anime ? animeJson : manga ? mangaJson : null
+    const { query, anime, manga, archive, genreFunc } = this.props
+    const json = anime ? animeJson : manga ? mangaJson : archive ? archiveJson : null
     if (!json) {
-      console.error('content called neither for manga, nor for anime.')
+      console.error('content called neither for manga, nor for anime, nor for archive.')
     }
     const data = sortBy(
       json
@@ -86,9 +94,13 @@ class Content extends Component {
           {...item}
           path={path} key={path}
           onGenreSelected={genre => genreFunc(genre)}
+          narrow={archive}
+          onClick={archive ? () => this.setState({ open: true, url: item.url }) : null}
         />
       )
     }
+
+    const { open, url } = this.state
 
     return (
       <div className='main-content'>
@@ -96,8 +108,19 @@ class Content extends Component {
           this.genreBar()
         }
         {list.length > 0
-          ? list
+          ? (
+            <div className={`${archive ? 'archive-list' : ''}`}>
+              {list}
+            </div>
+            )
           : <span className='inform-msg'> «<b>{query}</b>» հարցմամբ արդյունքներ չկան։ </span>}
+
+        <Custombox
+          pdf
+          doc={`https://docs.google.com/document/d/${url}/preview`}
+          on={open}
+          onClose={() => this.setState({ open: false })}
+        />
       </div>
     )
   }
--- src/content/util/Card.js
+++ src/content/util/Card.js
@@ -185,20 +185,17 @@ class Card extends Component {
 
   render () {
     const { path, title, complete, onClick, narrow } = this.props
+    const linkProps = onClick
+      ? {
+          onClick: () => onClick()
+        }
+      : {
+          to: path
+        }
 
     return (
       <div className={`information-card${narrow ? ' narrow' : ''}`}>
-        {onClick
-          ? (
-            <>
-              <Link onClick={() => onClick()}> {title} </Link>
-            </>
-            )
-          : (
-            <>
-              <Link to={path}> «{title}» </Link> <CompletedIndication complete={complete} />
-            </>
-            )}
+        <Link {...linkProps}> «{title}» </Link> <CompletedIndication complete={complete} />
         <div className='content'>
           <div className='img-container'>
 
