332057d64975f542af47bde8b1e3534d842645da
tado-mi <tadouchiha@gmail.com>
2021-04-24 21:48:15 -0400

    feature(archive): added data and code to `<Archive />`

--- src/content/Archive.js
+++ src/content/Archive.js
@@ -1,12 +1,48 @@
 import React, { Component } from 'react'
 
-import './sass/style.scss'
+import './sass/Archive.scss'
+
+import { archiveJson } from '../constants.js'
+
+import Custombox from './util/Custombox.js'
+import Card from './util/Card.js'
 
 class Archive extends Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+
+      open: false
+
+    }
+  }
+
   render () {
+    const { open, url } = this.state
+
+    const list = []
+    for (const item of archiveJson) {
+      item.path = `/archive/${item.path}`
+      list.push(
+        <Card narrow onClick={() => this.setState({ open: true, url: item.url })} {...item} />
+      )
+    }
+
     return (
       <div className='main-content'>
-        արխիվ
+
+        <div className='archive-list'>
+          {list}
+        </div>
+
+        <Custombox
+          pdf
+          doc={`https://docs.google.com/document/d/${url}/preview`}
+          on={open}
+          onClose={() => this.setState({ open: false })}
+        />
+
       </div>
 
     )
